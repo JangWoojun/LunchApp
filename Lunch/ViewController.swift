b348ae2158484eb6f5bdd26790a5df7228a4b36b
JangWoojun <woojun0107@naver.com>
2022-12-22 14:42:47 +0900

    FEAT : 음식 종류 선택이 안됐을 때 보여줄 메시지 추가
    음식 종류가 선택이 안됐을 때 알려줄 토스트 메시지 추가

--- Lunch/ViewController.swift
+++ Lunch/ViewController.swift
@@ -64,7 +64,25 @@ class ViewController: UIViewController {
                 break
             }
         } else {
-            
+            showToast(message: "아직 음식 종류가 선택되지 않았어요!")
+        }
+        
+        func showToast(message : String, font: UIFont = UIFont.systemFont(ofSize: 14.0)) {
+            let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 125, y: self.view.frame.size.height-100, width: 250, height: 35))
+            toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+            toastLabel.textColor = UIColor.white
+            toastLabel.font = font
+            toastLabel.textAlignment = .center;
+            toastLabel.text = message
+            toastLabel.alpha = 1.0
+            toastLabel.layer.cornerRadius = 10;
+            toastLabel.clipsToBounds  =  true
+            self.view.addSubview(toastLabel)
+            UIView.animate(withDuration: 3.0, delay: 0.1, options: .curveEaseOut, animations: {
+                toastLabel.alpha = 0.0
+            }, completion: {(isCompleted) in
+                toastLabel.removeFromSuperview()
+            })
         }
         
     }
